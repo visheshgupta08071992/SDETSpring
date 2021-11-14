bc9ac38cfc96452ec661fad80bd8bfb2600fa6ab
Gupta <Visheshkumar.Gupta@msci.com>
2021-11-14 15:41:22 +0530

    Created Spring APIS using Hashmap

---
+++ src/main/java/com/SDETSpring/SDETSpring/Entity/Country.java
@@ -0,0 +1,15 @@
+package com.SDETSpring.SDETSpring.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Country {
+
+    int id;
+    String countryname;
+    String countryCapital;
+}
---
+++ src/test/java/com/SDETSpring/SDETSpring/SdetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SDETSpring.SDETSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdetSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
